cd9aa412b84731f3200e80efdb0b4d4c2f6b797d
iarley <7iarley7@gmail.com>
2024-02-19 15:16:50 -0300

    Feat: Cria migrations para ACL

---
+++ database/migrations/2024_02_19_181140_create_permissions_roles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permissions_roles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->bigInteger('permission_id')->unsigned();
+            $table->foreign('permission_id')
+                ->references('id')
+                ->on('permissions')->onDelete('cascade');
+
+            $table->bigInteger('role_id')->unsigned();
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permissions_roles');
+    }
+};
