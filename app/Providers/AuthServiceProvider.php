bf47c10c9ea76dea201581f925e96bcad203b75e
iarley <7iarley7@gmail.com>
2024-02-19 23:42:19 -0300

    Feat: Modificando Gate para verificar permissao do usuario

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Exception;
+use App\Models\User;
+use App\Models\Permission;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -18,9 +22,30 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * Register any authentication / authorization services.
+     * 
+     * Adicionando papeis
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        $this->registerPolicies();
+
+        try {
+            // trazendo permissoes
+            $permissions = Permission::with('roles')->get();
+            // criando politica de permissao com gate
+            // verifica se um uusario tem certa permissao
+            foreach ($permissions as $permission) {
+                Gate::define($permission->name, function (User $user) use ($permission) {
+                    foreach ($user->roles as $role) {
+                        if ($role->permissions->contains('name', $permission->name)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                });
+            }
+        } catch (Exception $e) {
+            return false;
+        }
     }
 }
